01d0b3e44cf98daf0eb6a4220907d2c631d02af6
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-25 16:28:46 +0100

    add first part

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+//chiedo all'utente come si chiama
+const fullName = prompt('Come ti chiami?')
+console.log(fullName);
+
+//richiamo il nome dell'utente nella mia pagina html
+//seleziono l'elemento della pagina
+const elementName = document.querySelector('.nomeCognomeVuoto')
+    //richiamo il nome
+elementName.innerHTML = fullName
+
+
+
+//chiedo all'utente i km da percorrere
+const kmDaPercorrere = prompt('Quanti km devi percorrere?')
+console.log(kmDaPercorrere);
+
+//richiamo i km nella mia pagina html
+//seleziono l'elemento della pagina
+const elementKm = document.querySelector('.km')
+    //richiamo il nome
+elementKm.innerHTML = kmDaPercorrere
