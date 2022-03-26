c7e040353f4648f23efb1bc85550fe716927bdb2
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-26 17:39:30 +0100

    initial X

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,47 @@
-//chiedo all'utente come si chiama
-const fullName = prompt('Come ti chiami?')
-console.log(fullName);
+//Faccio scrivere all'utente il suo nome con html
+//Creo il btn del click in html
+//Prendo il nome scritto dall'utente e lo metto dentro il div.testo_nome al click
+document.getElementById("btn_click").addEventListener("click", scrivinome)
 
-//richiamo il nome dell'utente nella mia pagina html
-//seleziono l'elemento della pagina
-const elementName = document.querySelector('.nomeUser')
-    //richiamo il nome
-elementName.innerHTML = fullName
+function scrivinome() {
+    const elementNome = document.getElementById('nome_utente').value;
+    document.getElementById('testo_nome').innerHTML = elementNome;
+}
 
+//Faccio scrivere all'utente quanti km vuole percorrere con html
+//Prendo i km scritti dall'utente e li metto dentro una variabile (kmFatti)
+const kmFatti = document.getElementById('km_utente').value;
 
-//chiedo all'utente i km da percorrere
-const kmDaPercorrere = prompt('Quanti km devi percorrere?')
-console.log(kmDaPercorrere);
-
-//richiamo i km nella mia pagina html
-//seleziono l'elemento della pagina
-const elementKm = document.querySelector('.km')
-    //richiamo il nome
-elementKm.innerHTML = kmDaPercorrere
+//Chiedo all'utente lo status con html e lo salvo in una variabile
+const elementStatus = document.getElementById('eta');
 
 
+/*
+CALCOLO IL PREZZO
+*/
+//Creo gli sconti
+const scontoMinorenni = 0.2;
+const scontoOver = 0.4;
 
+//Creo una variabile (euroKm) che indica il P. unitario al km
+const euroKm = 0.21;
 
-//Voglio estrarre l'imput (nome) dell'utente
-//seleziono l'elemento della pagina
-//const elementuser = document.querySelector()
+//Calcolo il prezzo della corsa senza sconti
+const prezzoCorsa = kmFatti * euroKm;
+
+//Creo il prezzo finale
+let prezzoFinale = prezzoCorsa;
+
+if (elementStatus == "minorenne") {
+    document.getElementById('tipologia').innerHTML = 'sconto minorenni'
+    prezzoFinale = prezzoCorsa - (prezzoCorsa * scontoMinorenni);
+    document.getElementById('finale').innerHTML = 'euro' + prezzoFinale.toFixed(2)
+} else if (elementStatus == "over65") {
+    document.getElementById('tipologia').innerHTML = 'sconto over'
+    prezzoFinale = prezzoCorsa - (prezzoCorsa * scontoOver);
+    document.getElementById('finale').innerHTML = 'euro' + prezzoFinale.toFixed(2)
+} else {
+    document.getElementById('tipologia').innerHTML = 'nessun sconto'
+    prezzoFinale = prezzoCorsa;
+    document.getElementById('finale').innerHTML = 'euro' + prezzoFinale.toFixed(2)
+}
